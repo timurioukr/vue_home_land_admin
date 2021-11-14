338d4b16a73150ba130b505726b3b964fbe4d936
Timur <timurioukr@gmail.com>
2021-11-14 22:15:34 +0200

    Add crm el

--- src/views/AdminPage.vue
+++ src/views/AdminPage.vue
@@ -1,6 +1,67 @@
 <template>
   <v-template>
     <SideBar />
+    <v-container>
+      <v-app-bar color="rgba(0,0,0,0)" flat>
+        <v-text-field 
+          label="Search..." 
+          class="pt-5" 
+          filled 
+          prepend-inner-icon="mdi-magnify"
+          dense 
+          solo 
+          flat 
+          background-color="grey lighten-4" 
+          rounded
+        ></v-text-field>
+        <v-spacer></v-spacer>
+        <v-btn icon dark>
+          <v-icon color="black">far fa-bell</v-icon>
+        </v-btn>
+        <v-select 
+          class="mt-7" 
+          :items="items" 
+          v-model="select" 
+          item-text="state" 
+          color="black" 
+          solo 
+          flat 
+          style="max-width: 7%;" item-color="grey darken-2"
+        ></v-select>
+        <span text color="#878A94">School</span>
+        <v-avatar size='30' class="ml-2">
+          <v-img src="https://cdn.vuetifyjs.com/images/lists/2.jpg"></v-img>
+        </v-avatar>
+      </v-app-bar>
+      <v-row>
+        <v-col cols="12" sm="8">
+          <v-card class="mx-4 rounded-xl pa-4" color="grey lighten-3" flat>
+            <v-row>
+              <v-col cols="12" sm="8">
+                <v-list-item three-line>
+                  <v-list-item-content>
+                  <v-list-item-title class="text-h4" mb-1>School</v-list-item-title>
+                  <v-list-item-subtitle>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil eum distinctio iure animi. Cumque necessitatibus eaque eligendi commodi veritatis. Repellendus blanditiis iusto, eaque doloribus sunt perferendis laborum nobis cumque dicta!</v-list-item-subtitle>
+                  <v-list-item-subtitle>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil eum distinctio iure animi. Cumque necessitatibus eaque eligendi!</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+                <v-card-actions>
+                  <v-btn class="px-3" color="black" dark>View detail</v-btn>
+                </v-card-actions>
+              </v-col>
+              <v-col cols="12" sm="4">
+                <v-img 
+                src="../assets/img/EidosCRM/teacher.png" 
+                class="mt-n16" 
+                style=" transform: rotateY(180deg)"
+                ></v-img>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="4"></v-col>
+      </v-row>
+    </v-container>
   </v-template>
 </template>
 
@@ -13,7 +74,17 @@ export default {
     SideBar
   },
   data: () => ({
-
+    select: {state: 'EN'},
+    items: ['EN', 'ES', 'DZ'],
+    text: 'weekly',
+    width: 2,
+    radius: 10,
+    padding: 8,
+    lineCap: 'round',
+    value: [0,2,5,9,5,10,3,5,0,0,1,8,2,9,0],
+    fill: false,
+    type: 'trend',
+    autoLineWidth: false,
   })
 }
 </script>
