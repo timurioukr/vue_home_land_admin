a2c3fc0fdda00e13e03af58f3b6f5e28cd5c8b41
Timur <timurioukr@gmail.com>
2022-02-22 22:26:32 +0200

    add

---
+++ src/components/Popup.vue
@@ -0,0 +1,85 @@
+<template>
+  <div v-if="isOpen" class="backdrop" @click="close">
+    <div class="popup" @click.stop>
+      <h1>Внимание!</h1>
+      <hr />
+      <slot></slot>
+      <hr />
+      <div class="footer">
+        <slot name="actions" :close="close" :confirm="confirm">
+          <button @click="close">Отмена</button>
+          &nbsp;
+          <button @click="confirm">Ok</button>
+        </slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isOpen: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  emits: {
+    ok: null,
+    close: null,
+  },
+
+  mounted() {
+    document.addEventListener("keydown", this.handleKeydown);
+  },
+  beforeDestroy() {
+    document.removeEventListener("keydown", this.handleKeydown);
+  },
+
+  methods: {
+    handleKeydown(e) {
+      if (this.isOpen && e.key === "Escape") {
+        this.close();
+      }
+    },
+    close() {
+      this.$emit("close");
+    },
+    confirm() {
+      this.$emit("ok");
+    },
+  },
+};
+</script>
+
+<style>
+.popup {
+  top: 50px;
+  padding: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  position: fixed;
+  z-index: 101;
+  background-color: white;
+  border-radius: 10px;
+}
+
+.popup h1 {
+  text-align: center;
+  margin: 0;
+}
+
+.backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.8);
+  z-index: 100;
+}
+
+.footer {
+  text-align: right;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -31,6 +31,26 @@
         This is a new modal footer.
       </template>
     </Modal>
+    <div>
+      <v-btn type="button" @click="openPopup">Открыть окно</v-btn>
+      <Popup
+          :is-open="isPopupOpen"
+          @ok="popupConfirmed"
+          @close="isPopupOpen = false"
+      >
+        Вы действительно хотите освоить правильные подходы к проектированию систем
+        во Vue?
+        <template #actions="{ confirm }">
+          Напишите
+          <input :placeholder="$options.CONFIRMATION_TEXT" v-model="confirmation" />
+          <v-btn
+              type="button"
+              class="info"
+              @click="confirm"
+              :disabled="!isConfirmationCorrect">OK</v-btn>
+        </template>
+      </Popup>
+    </div>
   </div>
 </template>
 
@@ -42,12 +62,14 @@ import ThreeBlock from "../components/Homepage/Three.vue"
 import FourBlock from "../components/Homepage/Four.vue"
 import SixBlock from "../components/Homepage/Fife.vue"
 import SevenBlock from "../components/Homepage/Six.vue"
-import Slots from "@/components/Homepage/Slots";
-import Modal from "@/components/Modal";
+import Slots from "@/components/Homepage/Slots"
+import Modal from "@/components/Modal"
+import Popup from "@/components/Popup"
 
 export default {
   name: "Home",
   components: {
+    Popup,
     Modal,
     Slots,
     FirstBlock, 
@@ -59,10 +81,26 @@ export default {
     },
   data() {
     return {
+      isPopupOpen: false,
+      confirmation: "",
       isModalVisible: false,
     };
   },
+  CONFIRMATION_TEXT: "ПОДТВЕРЖДАЮ",
+  computed: {
+    isConfirmationCorrect() {
+      return this.confirmation === this.$options.CONFIRMATION_TEXT;
+    },
+  },
   methods: {
+    openPopup() {
+      this.confirmation = "";
+      this.isPopupOpen = true;
+    },
+    popupConfirmed() {
+      alert("Confirmed!");
+      this.isPopupOpen = false;
+    },
     showModal() {
       this.isModalVisible = true;
     },
